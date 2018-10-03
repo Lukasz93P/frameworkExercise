efadeddfc1c124d68f98ff55d5b8aa1ed657d141
Lukasz93P <lukasz9.3@interia.pl>
2018-10-03 17:44:53 +0200

    xml config

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 declare(strict_types=1);
 
-use Core\Core;
-use Core\ControllerLoaders\CoreControllerLoader;
 use Core\Session\Session;
-use Core\Methodselection\CoreMethodSelector;
-use Core\Controllercalling\CoreControllerCaller;
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'secrets.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'Config.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR .  'core' . DIRECTORY_SEPARATOR .'config' . DIRECTORY_SEPARATOR . 'config.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 Session::start(null, 300);
-$core = new Core(new CoreControllerLoader(), new CoreMethodSelector(), new CoreControllerCaller());
+
+$xmlConfigPath = __DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.xml';
+$config = new Config($xmlConfigPath);
+$config->config();
+$core = $config->getObject('Core');
---
+++ src/core/models/AbstractModel.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018-10-03
+ * Time: 17:42
+ */
+
+class AbstractModel
+{
+
+}
